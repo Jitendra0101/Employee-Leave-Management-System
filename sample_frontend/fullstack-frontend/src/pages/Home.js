a6c2aae798996dab80bd2b21e2c565f1be48cc68
Vedant Pingale <vedantpingale1@gmail.com>
2024-02-12 18:32:07 +0530

    Added UI Elements and Configured for MongoDB

--- sample_frontend/fullstack-frontend/src/pages/Home.js
+++ sample_frontend/fullstack-frontend/src/pages/Home.js
@@ -1,64 +1,85 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
+import NavbarAD from '../layout/AdminLayout/NavbarAD';
+import '../CSS/Login.css'
+import { listWorkers } from '../service/WorkerService';
 
 export default function Home() {
-    const [users, setUsers] = useState([]);
+    // const [users, setUsers] = useState([]);
+    const [workers, setWorkers] = useState([]);
 
-    const {id}= useParams();
+    useEffect(() => {
+        listWorkers().then((resp) => {
+            setWorkers(resp.data);
+        }).catch(error => {
+            console.error(error);
+        });
+    }, []);
+    // const {id}= useParams();
 
-    useEffect(()=>{
-        loadUsers();
-    },[])
+    // useEffect(()=>{
+    //     loadUsers();
+    // },[])
 
-    const loadUsers=async()=>{
-        const result = await axios.get("http://localhost:8080/users")
-        setUsers(result.data);
 
-    }
+    // useEffect(() => {
+    //     listWorkers().then((resp) => {
+    //         setWorkers(resp.data);
+    //     }).catch(error => {
+    //         console.error(error);
+    //     });
+    // }, []);
 
-    const deleteUser= async (id)=>{
-      await  axios.delete(`http://localhost:8080/user/${id}`)
-      loadUsers();
-    }
+    // const loadUsers=async()=>{
+    //     const result = await axios.get("http://localhost:8080/users")
+    //     setUsers(result.data);
+
+    // }
+
+    // const deleteUser= async (id)=>{
+    //   await  axios.delete(`http://localhost:8080/user/${id}`)
+    //   loadUsers();
+    // }
 
   return (
-    <div className='container'>
+    <div className=''>
+    <NavbarAD />
+    <div className='container '>
+      
         <div className='py-4'>
         <table className="table border shadow">
   <thead>
     <tr>
       <th scope="col">#</th>
       <th scope="col">Name</th>
-      <th scope="col">Username</th>
       <th scope="col">email</th>
-      <th scope="col">Action</th>
+      <th scope="col">Designation</th>
+      <th scope="col">Join-Date</th>
     </tr>
   </thead>
   <tbody>
     {
-        users.map((user,index)=>(
+        workers.map((user,index)=>(
             <tr>
-            <th scope="row" key={index}>{index+1}</th>
-            <td>{user.name}</td>
-            <td>{user.username}</td>
+            <th scope="row" key={index}>{user.id}</th>
+            <td>{user.userName}</td>
             <td>{user.email}</td>
-            <td>
+            <td>{user.designation}</td>
+            <td>{user.joinDate}</td>
+            {/* <td>
                 <Link className='btn btn-primary mx-2' to={`/viewUser/${user.id}`}>View</Link>
                 <Link className='btn btn-outline-primary mx-2' to={`/editUser/${user.id}`}>Edit</Link>
                 <button className='btn btn-danger mx-2' onClick={(e)=>deleteUser(user.id)} >Delete</button>
-            </td>
+            </td> */}
             </tr>
         ))
     }
-
-
-    
-       
   </tbody>
 </table>
         </div>
 
+    </div>
     </div>
   )
 }
