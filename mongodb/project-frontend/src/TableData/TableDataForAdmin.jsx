b04ec68a6cd3e4ed1fc417df35ac2c7ac161c2f4
Jitendra <sykesbmtholiver@gmail.com>
2024-02-13 15:38:18 +0530

    added view and delete function to button

--- mongodb/project-frontend/src/TableData/TableDataForAdmin.jsx
+++ mongodb/project-frontend/src/TableData/TableDataForAdmin.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
+import { listWorkers } from '../services/WorkerService';
 
-const TableDataForAdmin = ({ workersList }) => {
+const TableDataForAdmin = ({ workersList, onDelete }) => {
+
+    const [workers, setWorkers] = useState([]);
+    const { id } = useParams();
+
+    useEffect(() => {
+        loadWorkers();
+    }, [])
+
+    const loadWorkers = async () => {
+        try {
+            const response = await listWorkers();
+            setWorkers(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // Inside TableDataForAdmin component
+    const handleDelete = async (id) => {
+        try {
+            await onDelete(id);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div>
             <h2 className='text-center' style={{ marginBottom: '40px', marginTop: '20px' }}>Admin DashBoard</h2>
@@ -9,10 +37,12 @@ const TableDataForAdmin = ({ workersList }) => {
                     <thead>
                         <tr>
                             <th>Sr. No</th>
-                            <th>Worker user name</th>
-                            <th>Worker email</th>
-                            <th>Worker designation</th>
-                            <th>Worker joining date</th>
+                            <th>user name</th>
+                            <th>email</th>
+                            <th>designation</th>
+                            <th>joining date</th>
+                            <th style={{ textAlign: 'center' }}>Actions</th>
+
                         </tr>
                     </thead>
                     <tbody>
@@ -23,12 +53,23 @@ const TableDataForAdmin = ({ workersList }) => {
                                 <td>{worker.email}</td>
                                 <td>{worker.designation}</td>
                                 <td>{worker.joinDate}</td>
+                                <td>
+                                    <Link className="btn btn-primary" to={`/viewworker/${worker.id}`} style={{ marginLeft: '10px', marginRight: '20px', borderRadius: '9px', width: '90px', height: '45px', fontSize: '18px' }}>
+                                        View
+                                    </Link>
+                                    <Link className="btn btn-secondary" to={`/editworker/${worker.id}`} style={{ marginRight: '20px', borderRadius: '9px', width: '90px', height: '45px', fontSize: '18px' }}>
+                                        Edit
+                                    </Link>
+                                    <button className="btn btn-danger" onClick={(e) => handleDelete(worker.id)} style={{ marginRight: '10px', borderRadius: '9px', width: '90px', height: '45px', fontSize: '18px' }}>
+                                        Delete
+                                    </button>
+                                </td>
                             </tr>
                         )}
                     </tbody>
                 </table>
             </div>
-        </div>
+        </div >
 
     )
 }
