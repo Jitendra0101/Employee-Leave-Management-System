f8e0e86bbf5a96202b27f8db57d9405ede6c717c
Jitendra Lakka {DEDSEC} <sykesbmtholiver@gmail.com>
2024-02-19 11:33:01 +0530

    Delete mongodb/project-frontend directory

--- mongodb/project-frontend/src/services/GetWorkerById.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const GetWorkerById = (id) => {
-    const Rest_Api_base_URL = `http://localhost:6900/api/workers/${id}`;
-    return axios.get(Rest_Api_base_URL);
-};
